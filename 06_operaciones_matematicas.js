2d9d7c129cff0fa0f0c24892c2d7f0bef69ea09f
iecgerman <80138974+iecgerman@users.noreply.github.com>
2023-08-18 02:41:36 +0000

    Initial commit

---
+++ 06_operaciones_matematicas.js
@@ -0,0 +1,10 @@
+// https://platzi.com/clases/4204-pensamiento-logico-desafios/56218-playgrounds-operacion-matematica/
+
+function solution(num1, num2) {
+let solution = (num1 - num2) + "" + (num1 + num2)
+
+console.log(solution) 
+}
+
+solution(5, 4) // 19
+solution(21, 5) // 1626 
